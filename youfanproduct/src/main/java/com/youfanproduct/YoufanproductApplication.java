3e90ec807e0aef187f039fe3ae6de8bf47d1531f
shermain123 <shermain123@163.com>
2019-07-19 23:20:30 +0800

    shermain

---
+++ youfanproduct/src/main/java/com/youfanproduct/YoufanproductApplication.java
@@ -0,0 +1,13 @@
+package com.youfanproduct;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YoufanproductApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(YoufanproductApplication.class, args);
+	}
+
+}
---
+++ youfanproducttypes/src/test/java/com/cn/youfanproducttypes/YoufanproducttypesApplicationTests.java
@@ -0,0 +1,16 @@
+package com.cn.youfanproducttypes;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class YoufanproducttypesApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
